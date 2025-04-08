28eb92efa7b9d52eaa5bfe9df7a8edd448a9183b
Miguel Abela <abelamiguel@gmail.com>
2025-04-08 14:37:02 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# play_again_ma_swd62a
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/play_again_ma_swd62a/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.play_again_ma_swd62a
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
